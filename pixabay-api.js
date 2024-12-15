a903255dc655edcf530629d9ebaf506d5a5aaa84
Vlad-Rzaiev <vlad.rzaev@gmail.com>
2024-12-15 19:29:40 +0000

    Deploying to gh-pages from @ Vlad-Rzaiev/goit-js-hw-11@5cfa535f4be13944daca272c8ae20e23404ebbe9 🚀

--- pixabay-api.js
+++
@@ -1,2 +0,0 @@
-import"./assets/modulepreload-polyfill-B5Qt9EMX.js";console.log("hello");
-//# sourceMappingURL=pixabay-api.js.map
--- render-functions.js
+++
@@ -1,2 +0,0 @@
-import"./assets/modulepreload-polyfill-B5Qt9EMX.js";console.log("hello");
-//# sourceMappingURL=render-functions.js.map
