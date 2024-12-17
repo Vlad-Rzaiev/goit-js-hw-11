34425cecbdae5e1a0bb54a9bd9b941818b7e4d67
Vladyslav Rzaiev <vlad.rzaev@gmail.com>
2024-12-17 19:54:31 +0100

    import uesrInputValue

--- src/js/pixabay-api.js
+++ src/js/pixabay-api.js
@@ -1,9 +1,11 @@
 import { getUserInput } from '../main';
 console.log(getUserInput());
 
+const userInputValue = getUserInput();
+
 const options = {
   key: '47679134-c77d37d01e499358209d43473',
-  q: document,
+  q: userInputValue,
   image_type: 'photo',
   orientation: 'horizontal',
   safesearch: true,
